2ba447b4c1d81219a16357ccc22717032de27c23
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2024-03-12 22:01:50 +0530

    lucia server hooks && adapted setup

--- src/lib/db/db.sever.ts
+++ src/lib/db/db.sever.ts
@@ -1,9 +1,8 @@
-// db/db.server.ts
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
 import { dev } from '$app/environment';
 
 const client = dev ? postgres(DATABASE_URL) : postgres(DATABASE_URL, { ssl: 'require' });
-// const client = postgres(DATABASE_URL);
+
 export const db = drizzle(client, {});
