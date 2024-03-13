c3432d1ed9f370cc9f3f141719a18f30913b7890
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2024-03-13 09:03:03 +0530

    login logic impl

--- src/lib/db/db.sever.ts
+++ src/lib/db/db.sever.ts
@@ -3,10 +3,11 @@ import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
 import { dev } from '$app/environment';
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
+import * as userSchema from './schema/user.schema';
 import { userTable, sessionTable } from './schema/user.schema';
 
 const client = dev ? postgres(DATABASE_URL) : postgres(DATABASE_URL, { ssl: 'require' });
 
-export const db = drizzle(client, {});
+export const db = drizzle(client, { schema: { ...userSchema } });
 
 export const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,9 +1,7 @@
 import { lucia } from '$lib/server/auth';
 import { fail, redirect } from '@sveltejs/kit';
-import { generateId } from 'lucia';
 import { Argon2id } from 'oslo/password';
 import { db } from '$lib/db/db.sever';
-import { userTable } from '$lib/db/schema/user.schema';
 
 import type { Actions } from './$types';
 
@@ -12,47 +10,37 @@ export const actions: Actions = {
 		const formData = await event.request.formData();
 		const email = formData.get('email');
 		const password = formData.get('password');
-		const fullName = formData.get('fullName');
 
-		if (!fullName || typeof fullName !== 'string') {
+		if (typeof email !== 'string' || !email) {
 			return fail(400, {
-				message: 'Name required'
+				message: 'Invalid email'
 			});
 		}
-
-		if (!email || typeof email !== 'string') {
-			return fail(400, {
-				message: 'Email requried'
-			});
-		}
-
 		if (typeof password !== 'string' || password.length < 6 || password.length > 255) {
 			return fail(400, {
 				message: 'Invalid password'
 			});
 		}
 
-		const userId = generateId(15);
-		const hashedPassword = await new Argon2id().hash(password);
-
-		const user = await db.query.userTable.findFirst({
+		const existingUser = await db.query.userTable.findFirst({
 			where: (users, { eq }) => eq(users.email, email)
 		});
 
-		if (user) {
+		if (!existingUser) {
 			return fail(400, {
-				message: 'Email already exits'
+				message: 'Incorrect email or password'
 			});
 		}
 
-		await db.insert(userTable).values({
-			id: userId,
-			hashedPassword,
-			email,
-			fullName
-		});
+		const validPassword = await new Argon2id().verify(existingUser.hashedPassword, password);
 
-		const session = await lucia.createSession(userId, {});
+		if (!validPassword) {
+			return fail(400, {
+				message: 'Incorrect email or password'
+			});
+		}
+
+		const session = await lucia.createSession(existingUser.id, {});
 		const sessionCookie = lucia.createSessionCookie(session.id);
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
