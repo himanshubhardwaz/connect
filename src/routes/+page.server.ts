c817ddda970d2edfcd7b3110d819b92fb7c0047a
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2024-03-13 09:10:42 +0530

    sign out impl

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,25 @@
+import type { PageServerLoad, Actions } from './$types';
+import { redirect, fail } from '@sveltejs/kit';
+import { lucia } from '$lib/server/auth';
+
+export const load: PageServerLoad = async (event) => {
+	if (!event.locals.user) redirect(302, '/login');
+	return {
+		email: event.locals.user.email
+	};
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		if (!event.locals.session) {
+			return fail(401);
+		}
+		await lucia.invalidateSession(event.locals.session.id);
+		const sessionCookie = lucia.createBlankSessionCookie();
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+		redirect(302, '/login');
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1 class="text-red-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { enhance } from '$app/forms';
+</script>
+
+<p class="text-4xl text-blue-400">Currrently logged in</p>
+
+<form method="post" use:enhance>
+	<button>Sign out</button>
+</form>
